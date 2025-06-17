92d951f2d5cef6fcf7132f15bfb7cf8a9593a196
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-06-18 01:28:51 +0530

    understanding stream basics

--- src/main/java/org/streams/Streams.java
+++ src/main/java/org/streams/Streams.java
@@ -6,4 +6,13 @@ public class Streams {
     //A. a sequence of variables supporting different elements,
     // to process data in a simplified manner and to improve readability of the code.
 
+    // Streams consists of intermediate operations and terminal operations.
+    // A stream doesn't operate without a terminal operation
+
+    // intermediate - .filter(), .map(), .sorted(), .distinct(), .limit(), .skip()
+
+    // terminal - .collect(), .count(), .toList(), .forEach(), .reduce(), .anyMatch(), .allMatch(), .noneMatch()
+    // below are also terminal operations but short circuit. Meaning they stop execution as soon as they get the first result
+    // .findAny(), .findFirst()
+
 }
