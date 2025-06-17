6465f84e8b63ab8c04db246a77cf972fc32079f1
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-06-18 00:44:54 +0530

    Streams. What is a stream?

---
+++ src/main/java/org/streams/Streams.java
@@ -0,0 +1,9 @@
+package org.streams;
+
+public class Streams {
+
+    //Q. What are streams??
+    //A. a sequence of variables supporting different elements,
+    // to process data in a simplified manner and to improve readability of the code.
+
+}
