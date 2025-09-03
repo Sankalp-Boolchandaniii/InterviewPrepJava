db650a0a6f45e045b55c49e3804406b53a0e9627
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-09-04 02:19:37 +0530

    3Sum - definitely visit again

---
+++ src/main/java/org/neetcode/Three3Sum.java
@@ -0,0 +1,38 @@
+package org.neetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Three3Sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists=new ArrayList<>();
+        int n=nums.length;
+        if (n<3){
+            return lists;
+        }
+        Arrays.sort(nums);
+        for (int i=0; i<n; i++){
+            if ((i>0) && (nums[i]==nums[i-1]))
+                continue;
+            int l=i+1, r=n-1;
+            while (l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if (sum>0){
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    lists.add(List.of(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while ((l<r) && nums[l]==nums[l-1]) l++;
+                    while ((l<r) && nums[r]==nums[r+1]) r--;
+                }
+            }
+        }
+        return lists;
+    }
+
+}
