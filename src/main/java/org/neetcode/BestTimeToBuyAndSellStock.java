2531de024cb1a93085f33fc7c96c68897fc6bae3
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-09-07 02:20:02 +0530

    BestTimeToBuyAndSellStock

---
+++ src/main/java/org/neetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+package org.neetcode;
+
+import org.utils.SlidingWindow;
+import org.utils.TwoPointer;
+
+@SlidingWindow
+@TwoPointer
+public class BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        int maxdif=0, l=0, r=0, n=prices.length;
+        while (r<n){
+            int diff=prices[r]-prices[l];
+            if (diff>0){
+                maxdif=Math.max(diff, maxdif);
+            } else {
+                l=r;
+            }
+            r++;
+        }
+        return maxdif;
+    }
+
+}
