f64cef7e28a955aac08f4158eed4c5a430a64c3c
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-11-12 01:41:59 +0530

    max from the list

--- src/main/java/org/streams/Practice0.java
+++ src/main/java/org/streams/Practice0.java
@@ -1,8 +1,12 @@
 package org.streams;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Practice0 {
+
+    private static List<Integer> list=List.of(1,2,3,4,5);
 
     public static void main(String[] args) {
         q1();
@@ -10,7 +14,6 @@ public class Practice0 {
 
     // get the sum of all the numbers in the stream
     static void q1(){
-        List<Integer> list=List.of(1,2,3,4,5);
         Integer sum = list.stream().reduce(0, (a, b) -> a + b);
         System.out.println(sum);
 
@@ -19,5 +22,11 @@ public class Practice0 {
 
         sum = list.stream().reduce((a, b) -> a + b).get();
         System.out.println(sum);
+    }
+
+    // max from the list
+    static void q2(){
+        Integer max = list.stream().reduce((a, b) -> Integer.max(a, b)).get();
+        System.out.println(max);
     }
 }
