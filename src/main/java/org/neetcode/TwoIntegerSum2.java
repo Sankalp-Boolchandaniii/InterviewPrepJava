c9e1b81d1bb70fdf67727ee95357953c93876790
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-12-23 00:51:40 +0530

    two sum in linear time

--- src/main/java/org/neetcode/TwoIntegerSum2.java
+++ src/main/java/org/neetcode/TwoIntegerSum2.java
@@ -1,7 +1,9 @@
 package org.neetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TwoIntegerSum2 {
 
@@ -20,4 +22,16 @@ public class TwoIntegerSum2 {
         return new int[] {};
     }
 
+
+    public static int[] twoSumBetter(int[] numbers, int target) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for (int i=0; i<numbers.length; i++){
+            int diff=target-numbers[i];
+            if (map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            }
+            map.put(numbers[i], i);
+        }
+        return new int[]{};
+    }
 }
