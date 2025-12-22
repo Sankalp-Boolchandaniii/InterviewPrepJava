b3598f828595d20403190447b7d0496e3acab276
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-12-23 00:52:46 +0530

    two sum

--- src/main/java/org/neetcode/TwoIntegerSum2.java
+++ src/main/java/org/neetcode/TwoIntegerSum2.java
@@ -8,22 +8,6 @@ import java.util.Map;
 public class TwoIntegerSum2 {
 
     public static int[] twoSum(int[] numbers, int target) {
-        List<Integer> list = Arrays.stream(numbers).boxed().toList();
-        int len=numbers.length;
-        for (int i=0;i<len;i++) {
-            int diff = target - numbers[i];
-            if (numbers[i] == diff) {
-                numbers[i] = Integer.MIN_VALUE;
-            }
-            if (list.contains(diff)) {
-                return new int[] { i + 1, list.indexOf(diff) + 1 };
-            }
-        }
-        return new int[] {};
-    }
-
-
-    public static int[] twoSumBetter(int[] numbers, int target) {
         Map<Integer, Integer> map=new HashMap<>();
         for (int i=0; i<numbers.length; i++){
             int diff=target-numbers[i];
