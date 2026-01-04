eb18e3a283604ecc921eb45e2cabd924a9339e85
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-01-04 23:54:15 +0530

    PermutationInString - self and notes

--- src/main/java/org/neetcode/PermutationInString.java
+++ src/main/java/org/neetcode/PermutationInString.java
@@ -31,4 +31,32 @@ public class PermutationInString {
         // Check the final window after the loop finishes
         return Arrays.equals(s1Arr, s2Arr);
     }
+
+
+    public boolean checkInclusionSelf(String s1, String s2) {
+        if (s1.length()>s2.length()) return false;
+        int[] s1Arr=new int[26];
+        int[] s2Arr=new int[26];
+        for (int i=0;i<s1.length();i++){
+            s1Arr[s1.charAt(i)-'a']++;
+            s2Arr[s2.charAt(i)-'a']++;
+        }
+        if (Arrays.equals(s1Arr, s2Arr)) return true;
+        for (int i=0;i<s2.length()-s1.length();i++){
+            s2Arr[s2.charAt(i+s1.length())-'a']++;
+            s2Arr[s2.charAt(i)-'a']--;
+            if (Arrays.equals(s1Arr, s2Arr)) return true;
+        }
+        return false;
+    }
 }
+
+/*
+the main idea here is, we first create 2 arrays of size 26(since only small case letters are involved).
+We increment the indexes of the letters(based on ascii values)
+then we compare if the 2 arrays are equal.
+
+- first we run the first window  (size of s1 string) for both arrays with both strings
+- check if its same
+- if not we shift the window and compare for each window
+ */
