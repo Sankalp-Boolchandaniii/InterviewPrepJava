90479c00fb0fbd2fa6385d65e750a6494035a11a
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-01-05 00:07:52 +0530

    TwoSum - self and notes

--- src/main/java/org/neetcode/TwoIntegerSum2.java
+++ src/main/java/org/neetcode/TwoIntegerSum2.java
@@ -18,4 +18,27 @@ public class TwoIntegerSum2 {
         }
         return new int[]{};
     }
+
+
+    public static int[] twoSumPrac(int[] nums, int target) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            int diff=target-nums[i];
+            if (map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[2];
+    }
 }
+
+/*
+the main idea here is to find 2 numbers in a given array that equal to given target.
+- create a map that will store elements and their indexes
+- we iterate through the entire list
+- first we get the complement/difference of every element and find if we have it stored in our map with its index
+- if so, we return the indexes
+- if not we continue the execution and till it ends
+ */
