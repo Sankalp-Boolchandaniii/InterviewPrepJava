9a24514bdfea1f4e3bbe9c74e1c3a5f8a04ca907
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-06-13 17:25:44 +0530

    sorting - insertion sort

--- src/main/java/org/neetcode/Sorting.java
+++ src/main/java/org/neetcode/Sorting.java
@@ -4,7 +4,8 @@ public class Sorting {
 
     public static void main(String[] args) {
 //        bubbleSort(new int[]{5,3,4,2,1});
-        selectionSort(new int[]{5,3,4,2,1});
+//        selectionSort(new int[]{5,3,4,2,1});
+        insertionSort(new int[]{5,3,4,2,1});
     }
 
     static void bubbleSort(int[] arr){
@@ -37,6 +38,22 @@ public class Sorting {
             int temp=arr[smallestIdx];
             arr[smallestIdx]=arr[i];
             arr[i]=temp;
+        }
+        for (int i:arr){
+            System.out.println(i);
+        }
+    }
+
+    static void insertionSort(int[] arr){
+        int n=arr.length;
+        for (int i=1;i<n;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while (prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
         }
         for (int i:arr){
             System.out.println(i);
