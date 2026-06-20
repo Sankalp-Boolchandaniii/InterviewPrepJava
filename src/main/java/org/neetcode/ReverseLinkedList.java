f5006821f7de410c1b48c323da66d089b514dba7
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-06-20 16:58:07 +0530

    listNode changes

--- src/main/java/org/neetcode/ReverseLinkedList.java
+++ src/main/java/org/neetcode/ReverseLinkedList.java
@@ -1,5 +1,7 @@
 package org.neetcode;
 
+import org.utils.ListNode;
+
 public class ReverseLinkedList {
 
     public ListNode reverseList(ListNode head) {
@@ -15,14 +17,3 @@ public class ReverseLinkedList {
     }
 
 }
-
-
-
-// Definition for singly-linked list.
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
